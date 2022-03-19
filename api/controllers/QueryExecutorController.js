56bb112f1296986e90a46ee342201e0197388c91
senthilsweb <senthilsweb@gmail.com>
2022-03-18 21:53:20 -0400

    Added support for raw sql query executor

---
+++ api/controllers/QueryExecutorController.js
@@ -0,0 +1,39 @@
+﻿module.exports = {
+    //-----------------------------------------------
+    //
+    // POST http://localhost:1337/codegen/columndef?id=master&generatecode=true&inheritbase=true
+    //------------------------------------------------
+    execute: function (req, res) {
+        var qn = req.param('qn') //Query template name which has the raw or parameterized query in it.
+        var sql = !sails._.isUndefined(req.param('sql')) ? req.param('sql') : '' //Query template name which has the raw or parameterized query in it.
+        if (sql != '') {
+            sails.log.debug(sql);
+            sails.getDatastore().sendNativeQuery(sql, function (err, results) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(results.rows);
+                }
+            })
+
+        } else {
+            sails.hooks.views.render(sails.config.queries[qn], {}, function (err, view) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    sails.log.debug(sails.config.queries[qn]);
+                    sails.log.debug(view);
+                    sails.getDatastore().sendNativeQuery(view, function (err, results) {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            res.send(results.rows);
+                        }
+                    })
+                }
+            });
+        }
+
+    }
+};
+
---
+++ config/custom/queries.js
@@ -0,0 +1,4 @@
+﻿module.exports.queries = {
+    opening_balance: 'queries/opening_balance',
+    closing_balance: 'queries/closing_balance',
+};
